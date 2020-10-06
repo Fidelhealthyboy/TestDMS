7362ed0825500bc589c8a933ef0b28c7abbdbf40
Fidel <odokfidelis@gmail.com>
2020-10-06 18:45:09 +0100

    updated the python file

--- TestElasticsearchPy/TestElasticsearch.py
+++ TestElasticsearchPy/TestElasticsearch.py
@@ -1,4 +1,18 @@
 from elasticsearch import Elasticsearch
+import subprocess
+
+#Run the Mapreduce 
+
+mapProcess = subprocess.run([ 'yarn','jar','Storejsonesmr.jar', 'elasticsearch.EsDriver','ElasticsearchInput.txt', 'ElastictestOutput2'],stdout=subprocess.PIPE)
+
+print(mapProcess.stdout)
+
+if mapProcess.returncode == 0:
+	print('Map completed')
+
+#connect to Elasticsearch
 es = Elasticsearch(HOST="http://master",PORT=9200)
+
+#check Elasticsearch Index
 print(es.indices.exists(index="index1"))
 
